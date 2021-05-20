c41ee33df969f330c6e7aedcafe871c2b49a887e
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-05-21 02:32:34 +0800

    Add files via upload

---
+++ ether.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=eu1.ethermine.org:4444
+WALLET=0x86fc2bf54eeb39059638c91ade97322948882b9c.$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+cd "$(dirname "$0")"
+
+chmod +x ./arni && sudo ./arni -pool $POOL -wal $WALLET $@ -pass x -proto 2 -stales 0
